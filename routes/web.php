603aa04bbc2f9c721e85d3f49101c48e717b269d
t-takumi-h <t.takumi.h@gmail.com>
2021-09-05 00:34:24 +0900

    商品の購入機能を追加しました

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ Route::get('', 'ItemsController@showItems')->name('top');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 Route::get('items/{item}', 'ItemsController@showItemDetail')->name('item');
 
 Route::middleware('auth')->group(function () {
-    Route::get('items/{item}/buy', function () { return "商品購入画面";})->name('item.buy');
+    Route::get('items/{item}/buy', 'ItemsController@showBuyItemForm')->name('item.buy');
+    Route::post('items/{item}/buy', 'ItemsController@buyItem')->name('item.buy');
     Route::get('sell', 'SellController@showSellForm')->name('sell');
     Route::post('sell','SellController@sellItem')->name('sell');
 });
@@ -29,6 +29,6 @@ Route::middleware('auth')->group(function () {
 Route::prefix('mypage')->namespace('MyPage')->middleware('auth')->group(function(){
     Route::get('edit-profile','ProfileController@showProfileEditForm')->name('mypage.edit-profile');
     Route::post('edit-profile','ProfileController@editProfile')->name('mypage.edit-profile');
-
+    Route::get('bought-items', 'BoughtItemsController@showBoughtItems')->name('mypage.bought-items');
     Route::get('sold-items', 'SoldItemsController@showSoldItems')->name('mypage.sold-items');
 });
