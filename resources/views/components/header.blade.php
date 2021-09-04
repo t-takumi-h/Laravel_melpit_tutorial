48c4f95268095699a152f0ad5d739f10a8738c15
t-takumi-h <t.takumi.h@gmail.com>
2021-09-04 22:30:54 +0900

    商品を検索する機能を追加しました

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,8 +5,33 @@
             <img src="/images/logo-1.png" style="height: 39px;" alt="Melpit">
         </a>
 
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
              <ul class="navbar-nav ml-auto">
+                 <form class="form-inline" method="GET" action="{{ route('top') }}">
+                     <div class="input-group">
+                         <div class="input-group-prepend">
+                             <select class="custom-select" name="category">
+                                 <option value="">全て</option>
+                                 @foreach ($categories as $category)
+                                 <option value="primary:{{$category->id}}" class="font-weight-bold" {{ $defaults['category'] == "primary:" . $category->id ? 'selected' : ''}}>{{$category->name}}</option>
+                                     @foreach ($category->secondaryCategories as $secondary)
+                                     <option value="secondary:{{$secondary->id}}" {{ $defaults['category'] == "secondary:" . $secondary->id ? 'selected' : ''}}>　{{$secondary->name}}</option>
+                                     @endforeach
+                                 @endforeach
+                             </select>
+                         </div>
+                         <input type="text" name="keyword" class="form-control" value="{{$defaults['keyword']}}" aria-label="Text input with dropdown button" placeholder="キーワード検索">
+                         <div class="input-group-append">
+                             <button type="submit" class="btn btn-outline-dark">
+                                 <i class="fas fa-search"></i>
+                             </button>
+                         </div>
+                     </div>
+                 </form>
                  @guest
                      {{-- 非ログイン --}}
                      <li class="nav-item">
